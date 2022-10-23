3f85cde078d667c8e7406ffa9f72aa1c442580b9
Haxtar Soliterax <umutumutzercan@gmail.com>
2022-10-23 18:08:32 +0300

    Mysql Connection Class Created
    bağlantı sistemini ben yapıyorum haberiniz olsun

---
+++ Personel Vardiya Programı(Team)/Database Connection/MySqlConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personel_Vardiya_Programı_Team_.Database_Connection
+{
+    public class MySqlConnection
+    {
+
+        string connectionKey = null;
+
+        MySqlClient cli = new MySqlClient();
+
+        public MySqlConnection()
+        {
+
+        }
+
+        public MySqlConnection(string connectionKey)
+        {
+            this.connectionKey = connectionKey;
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void setConnectionKey(string connectionKey)
+        {
+            this.connectionKey = connectionKey;
+        }
+
+        public void generateConnectionKey(string hostIp, string dbname, string charset, string username, string password)
+        {
+            connectionKey = "mysql:host=" + hostIp + ";dbname=" + dbname + ";charset=" + charset + ";uid=" + username + ";pwd=" + password;
+        }
+
+       
+
+    }
+}
