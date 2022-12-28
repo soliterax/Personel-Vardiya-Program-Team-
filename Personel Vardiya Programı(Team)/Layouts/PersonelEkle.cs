4cf24e88a821f97061503af2a6dce4341f46f1fc
Haxtar Soliterax <umutumutzercan@gmail.com>
2022-12-28 23:18:23 +0300

    vardiyagösterme ekranı kodları bitti

---
+++ Personel Vardiya Programı(Team)/Layouts/PersonelEkle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Personel_Vardiya_Programı_Team_.SetupForm;
+
+namespace Personel_Vardiya_Programı_Team_.Layouts
+{
+    public partial class PersonelEkle : UserControl
+    {
+        public PersonelEkle()
+        {
+            InitializeComponent();
+        }
+
+        private void kaydet_Click(object sender, EventArgs e)
+        {
+            ConnectionMemory.conn.GetManager().SendData("insert into Personals(sicilno,kadro, unvan, name, surname, tcno, adres, telno, eposta ) values(" +
+                $"'{sicilno.Texts}', '{kadro.Texts}', '{unvan.Texts}', '{name.Texts}', '{surname.Texts}', '{tcno.Texts}', '{adres.Texts}', '{telno.Texts}', '{eposta.Texts}'" +
+                ")");
+            sicilno.Texts = "";
+            kadro.Texts = "";
+            unvan.Texts = "";
+            name.Texts = "";
+            surname.Texts = "";
+            tcno.Texts = "";
+            adres.Texts = "";
+            telno.Texts = "";
+            eposta.Texts = "";
+            this.Visible = false;
+            kaydetClicked.Invoke(null, null);
+        }
+
+        public EventHandler kaydetClicked;
+
+    }
+}
--- Personel Vardiya Programı(Team)/Layouts/UserVardiyaControl.cs
+++ Personel Vardiya Programı(Team)/Layouts/UserVardiyaControl.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Personel_Vardiya_Programı_Team_.SetupForm;
 
 namespace Personel_Vardiya_Programı_Team_.Layouts
 {
@@ -19,7 +20,18 @@ namespace Personel_Vardiya_Programı_Team_.Layouts
 
         private void UserVardiyaControl_Load(object sender, EventArgs e)
         {
+            dataGridView1.DataSource = ConnectionMemory.conn.GetManager().GetData("select * from Personals");
+            personelEkle1.kaydetClicked += kaydetClicked;
+            if (dataGridView1.Rows.Count < 1)
+                return;
+            id = (dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value != null) ? int.Parse(dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value.ToString()) : -1;
+        }
 
+        int id = -1;
+
+        private void kaydetClicked(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ConnectionMemory.conn.GetManager().GetData("select * from Personals");
         }
 
         private void customButton2_Click(object sender, EventArgs e) //geri butonu
@@ -33,6 +45,40 @@ namespace Personel_Vardiya_Programı_Team_.Layouts
 
         private void customButton1_Click(object sender, EventArgs e) // kaydet butonu
         {
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            
+                for (int i = 0; i < dataGridView1.Rows.Count; i++) {
+                    ConnectionMemory.conn.GetManager().SendData($"update Personals set sicilno = '{dataGridView1.Rows[i].Cells[1].Value.ToString()}', " +
+                        $"kadro='{dataGridView1.Rows[i].Cells[2].Value.ToString()}', unvan = '{dataGridView1.Rows[i].Cells[3].Value.ToString()}', name ='{dataGridView1.Rows[i].Cells[4].Value.ToString()}', surname='{dataGridView1.Rows[i].Cells[5].Value.ToString()}', tcno='{dataGridView1.Rows[i].Cells[6].Value.ToString()}', adres='{dataGridView1.Rows[i].Cells[7].Value.ToString()}', telno='{dataGridView1.Rows[i].Cells[8].Value.ToString()}', eposta='{dataGridView1.Rows[i].Cells[9].Value.ToString()}'" +
+                        $"where id= '{dataGridView1.Rows[i].Cells[0].Value.ToString()}'");
+                }
+            
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionMemory.conn.GetManager().SendData($"delete from Personals where id='{dataGridView1.SelectedRows[0].Cells[0].Value.ToString()}'");
+            dataGridView1.Rows.Remove(dataGridView1.SelectedRows[0]);
+            
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 1)
+                dataGridView1.Rows[0].Cells[0].Value = 0;
+            else if(dataGridView1.Rows.Count == 2)
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value = 1;
+            }
+            else
+            {
+                dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value = int.Parse(dataGridView1.Rows[dataGridView1.Rows.Count - 2].Cells[0].Value.ToString()) + 1;
+            }
+        }
+
+        private void customButton3_Click(object sender, EventArgs e)
+        {
+            personelEkle1.Visible = true;
 
         }
     }
